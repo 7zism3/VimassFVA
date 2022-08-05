06e1ab0c7148affa8334d85c2eeef1e0dc5de31e
Thuỷ Vimass <thuynv.vimass@gmail.com>
2022-08-05 18:34:25 +0700

    new

---
+++ VimassFVA/Main.Designer.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace VimassFVA
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_luuKM = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_soVi = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_luuDacTrungGiongNoi = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_xacThuc = new System.Windows.Forms.Button();
+            this.btn_khoiDongCamera = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_ketQuaDangKiKhuonMat = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lb_ketQuaDangKiKhuonMat);
+            this.panel1.Controls.Add(this.btn_luuKM);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txt_soVi);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.btn_luuDacTrungGiongNoi);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btn_xacThuc);
+            this.panel1.Controls.Add(this.btn_khoiDongCamera);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(904, 707);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_luuKM
+            // 
+            this.btn_luuKM.Location = new System.Drawing.Point(28, 264);
+            this.btn_luuKM.Name = "btn_luuKM";
+            this.btn_luuKM.Size = new System.Drawing.Size(122, 46);
+            this.btn_luuKM.TabIndex = 13;
+            this.btn_luuKM.Text = "Đăng kí khuôn mặt";
+            this.btn_luuKM.UseVisualStyleBackColor = true;
+            this.btn_luuKM.Click += new System.EventHandler(this.btn_dangKiKhuonMat_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(23, 551);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Không thành công";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(192, 166);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(672, 518);
+            this.panel2.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 484);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 521);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Kết quả xác thực: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Số ví";
+            // 
+            // txt_soVi
+            // 
+            this.txt_soVi.Location = new System.Drawing.Point(28, 214);
+            this.txt_soVi.Name = "txt_soVi";
+            this.txt_soVi.Size = new System.Drawing.Size(139, 20);
+            this.txt_soVi.TabIndex = 8;
+            this.txt_soVi.Text = "0357897375";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::VimassFVA.Properties.Resources.voice_recognition_icon_154433;
+            this.pictureBox2.Location = new System.Drawing.Point(610, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 56);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_luuDacTrungGiongNoi
+            // 
+            this.btn_luuDacTrungGiongNoi.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btn_luuDacTrungGiongNoi.Location = new System.Drawing.Point(742, 27);
+            this.btn_luuDacTrungGiongNoi.Name = "btn_luuDacTrungGiongNoi";
+            this.btn_luuDacTrungGiongNoi.Size = new System.Drawing.Size(122, 56);
+            this.btn_luuDacTrungGiongNoi.TabIndex = 5;
+            this.btn_luuDacTrungGiongNoi.Text = "Lưu đặc trưng giọng nói";
+            this.btn_luuDacTrungGiongNoi.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(38, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(455, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "* Tuân thủ các quy định của nhà nước về quyền bảo mật thông tin. \r\nVimass tuyệt đ" +
+    "ối không gửi bất kì dữ liệu khuôn mặt hay giọng nói nào của quý khách ra ngoài!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Phần mền lưu trữ đặc điểm nhận dạng khuôn mặt\r\n và giọng nói trên máy tính cá nhâ" +
+    "n của khách hàng.\r\nDùng nó để xác thực giao dịch\r\n";
+            // 
+            // btn_xacThuc
+            // 
+            this.btn_xacThuc.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btn_xacThuc.Location = new System.Drawing.Point(27, 431);
+            this.btn_xacThuc.Name = "btn_xacThuc";
+            this.btn_xacThuc.Size = new System.Drawing.Size(122, 51);
+            this.btn_xacThuc.TabIndex = 2;
+            this.btn_xacThuc.Text = "Xác thực";
+            this.btn_xacThuc.UseVisualStyleBackColor = true;
+            // 
+            // btn_khoiDongCamera
+            // 
+            this.btn_khoiDongCamera.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btn_khoiDongCamera.Location = new System.Drawing.Point(742, 97);
+            this.btn_khoiDongCamera.Name = "btn_khoiDongCamera";
+            this.btn_khoiDongCamera.Size = new System.Drawing.Size(122, 51);
+            this.btn_khoiDongCamera.TabIndex = 1;
+            this.btn_khoiDongCamera.Text = "Khởi động Camera";
+            this.btn_khoiDongCamera.UseVisualStyleBackColor = true;
+            this.btn_khoiDongCamera.Click += new System.EventHandler(this.btn_khoiDongCamera_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vimass";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_ketQuaDangKiKhuonMat
+            // 
+            this.lb_ketQuaDangKiKhuonMat.AutoSize = true;
+            this.lb_ketQuaDangKiKhuonMat.Location = new System.Drawing.Point(28, 329);
+            this.lb_ketQuaDangKiKhuonMat.Name = "lb_ketQuaDangKiKhuonMat";
+            this.lb_ketQuaDangKiKhuonMat.Size = new System.Drawing.Size(0, 13);
+            this.lb_ketQuaDangKiKhuonMat.TabIndex = 14;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 727);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Vimass - Face Voice Authentication";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_xacThuc;
+        private System.Windows.Forms.Button btn_khoiDongCamera;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_luuDacTrungGiongNoi;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_soVi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_luuKM;
+        private System.Windows.Forms.Label lb_ketQuaDangKiKhuonMat;
+    }
+}
+
---
+++ VimassFVA/Main.cs
@@ -0,0 +1,220 @@
+﻿using Luxand;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VimassFVA
+{
+    public partial class Main : Form
+    {
+        // program states: whether we recognize faces, or user has clicked a face
+        enum ProgramState { psRemember, psRecognize }
+        ProgramState programState = ProgramState.psRecognize;
+
+        bool needClose = false;
+        String cameraName;
+        string userName;
+        String TrackerMemoryFile = "tracker70.dat";
+        int mouseX = 0;
+        int mouseY = 0;
+
+        String fileName = "D://dataFVA.txt";
+
+        long[] IDs_Global;         // Danh sách khuôn mặt
+
+        bool batDauDangKi = false;
+
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            String keyLX = "uypkToItYK8NviZCLG+n9L6lgekJ5n5TWWkroruVGQf+Ku3pl30qunuTAYchw" +
+                "RC2MLKLubUspp+QI4BTUNHnCSiZbEcHmoOmxE4e/HTHik6bxM7I5V9LnggPEyD" +
+                "w8ga1Q4IfbBE5aR4mc9RgKRz6e9y/99phHELlXK03W1zur6w=";
+            if (FSDK.FSDKE_OK != FSDK.ActivateLibrary(keyLX))
+            {
+                MessageBox.Show("License Key Hết Hạn", "Lỗi rồi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            FSDK.InitializeLibrary();
+            FSDKCam.InitializeCapturing();
+
+            string[] cameraList;
+            int count;
+            FSDKCam.GetCameraList(out cameraList, out count);
+
+            if (0 == count)
+            {
+                MessageBox.Show("Vui lòng cho phép sử dụng camera", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            cameraName = cameraList[0];
+
+            FSDKCam.VideoFormatInfo[] formatList;
+            FSDKCam.GetVideoFormatList(ref cameraName, out formatList, out count);
+            System.Diagnostics.Debugger.Launch();
+            int VideoFormat = 0; // choose a video format
+            pictureBox1.Width = formatList[VideoFormat].Width;
+            pictureBox1.Height = formatList[VideoFormat].Height;
+            //this.Width = formatList[VideoFormat].Width + 48;
+            //this.Height = formatList[VideoFormat].Height + 96;
+
+
+        }
+
+        private void btn_khoiDongCamera_Click(object sender, EventArgs e)
+        {
+            khoiDongCamera();
+        }
+
+        private void khoiDongCamera()
+        {
+            //
+            int cameraHandle = 0;
+
+            int r = FSDKCam.OpenVideoCamera(ref cameraName, ref cameraHandle);
+            if (r != FSDK.FSDKE_OK)
+            {
+                MessageBox.Show("Lỗi mở camera thứ 1", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            int tracker = 0; 	// Tạo một Tracker
+            if (FSDK.FSDKE_OK != FSDK.LoadTrackerMemoryFromFile(ref tracker, TrackerMemoryFile)) // Cố gắng tải trạng thái trình theo dõi đã lưu
+                FSDK.CreateTracker(ref tracker); // Nếu không tải được, hãy tạo một trình theo dõi mới
+
+            int err = 0; // Đặt thông số nhận diện khuôn mặt thời gian thực
+            FSDK.SetTrackerMultipleParameters(tracker, "HandleArbitraryRotations=false; DetermineFaceRotationAngle=false; InternalResizeWidth=100; FaceDetectionThreshold=5;", ref err);
+
+            while (!needClose)
+            {
+                Int32 imageHandle = 0;
+                if (FSDK.FSDKE_OK != FSDKCam.GrabFrame(cameraHandle, ref imageHandle)) // Lấy khung hình hiện tại từ máy ảnh
+                {
+                    Application.DoEvents();
+                    continue;
+                }
+                FSDK.CImage image = new FSDK.CImage(imageHandle);
+
+                long[] IDs;         // Danh sách khuôn mặt
+                long faceCount = 0;
+                FSDK.FeedFrame(tracker, 0, image.ImageHandle, ref faceCount, out IDs, sizeof(long) * 1); // Tối đa 256 khuôn mặt được phát hiện
+                Array.Resize(ref IDs, (int)faceCount);
+                IDs_Global = IDs;
+                // Làm cho các điều khiển giao diện người dùng có thể truy cập được (để tìm xem người dùng có nhấp vào một khuôn mặt hay không)
+                Application.DoEvents();
+
+                Image frameImage = image.ToCLRImage();
+                Graphics gr = Graphics.FromImage(frameImage);
+
+                for (int i = 0; i < IDs.Length; ++i)
+                {
+                    // Lấy ra vị trí khuôn mặt
+                    FSDK.TFacePosition facePosition = new FSDK.TFacePosition();
+                    FSDK.GetTrackerFacePosition(tracker, 0, IDs[i], ref facePosition);
+
+                    int left = facePosition.xc - (int)(facePosition.w * 0.6);
+                    int top = facePosition.yc - (int)(facePosition.w * 0.5);
+                    int w = (int)(facePosition.w * 1.2);
+
+                    String name;
+                    int res = FSDK.GetAllNames(tracker, IDs[i], out name, 65536); // Tối đa 65536 ký tự
+
+                    if (FSDK.FSDKE_OK == res && name.Length > 0)
+                    {
+                        // Vẽ tên
+                        StringFormat format = new StringFormat();
+                        format.Alignment = StringAlignment.Center;
+
+                        gr.DrawString(name, new System.Drawing.Font("Arial", 16),
+                            new System.Drawing.SolidBrush(System.Drawing.Color.LightGreen),
+                            facePosition.xc, top + w + 5, format);
+                    }
+
+                    Pen pen = Pens.LightGreen;
+
+                    if (ProgramState.psRemember == programState || batDauDangKi)
+                    {
+                        if (FSDK.FSDKE_OK == FSDK.LockID(tracker, IDs[i]))
+                        {
+                            // get the user name
+
+                            userName = txt_soVi.Text;
+                            if (userName == null || userName.Length <= 0)
+                            {
+                                String s = "";
+                                FSDK.SetName(tracker, IDs[i], "");
+                                FSDK.PurgeID(tracker, IDs[i]);
+                            }
+                            else
+                            {
+                                FSDK.SetName(tracker, IDs[i], userName);
+                            }
+                            FSDK.UnlockID(tracker, IDs[i]);
+
+
+
+                        }
+                        lb_ketQuaDangKiKhuonMat.Text = "Đăng kí khuôn mặt thành công" +
+                            "\r\nCho số ví: " + userName;
+                        if (FSDK.SaveTrackerMemoryToFile(tracker, fileName) == FSDK.FSDKE_OK)
+                        {
+                            MessageBox.Show("OK");
+                        }
+
+                //batDauDangKi = false;
+            }
+
+                    gr.DrawRectangle(pen, left, top, w, w);
+
+
+                }
+                //programState = ProgramState.psRecognize;
+
+                // Hiển thị khung hiện tại
+                pictureBox1.Image = frameImage;
+                GC.Collect(); // Thu gom rác sau khi xóa 
+            }
+            FSDK.SaveTrackerMemoryToFile(tracker, TrackerMemoryFile);
+            FSDK.FreeTracker(tracker);
+
+            FSDKCam.CloseVideoCamera(cameraHandle);
+            FSDKCam.FinalizeCapturing();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            programState = ProgramState.psRemember;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            mouseX = 0;
+            mouseY = 0;
+        }
+
+        private void btn_dangKiKhuonMat_Click(object sender, EventArgs e)
+        {
+            batDauDangKi = true;
+            programState = ProgramState.psRemember;
+        }
+    }
+
+}
